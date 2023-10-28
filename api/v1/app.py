b5127d3522f831e101fc10f1ee914a80166d060a
AropetJ <aropetjoel32@gmail.com>
2023-10-28 16:53:11 +0300

    Attempted task 5

---
+++ api/v1/app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" A script that starts an API"""
+
+
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify, render_template, url_for
+from models import storage
+import os
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+app.url_map.strict_slashes = False
+
+port = os.getenv('HBNB_API_PORT', 5000)
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+
+@app.teardown_appcontext
+def close_db(exception):
+    """Closes the storage engine"""
+    storage.close()
+
+
+@app.errorhandler(Exception)
+def error_handler(e):
+    """Handles errors i.e the 404 page"""
+    if isinstance(e, HTTPException):
+        if type(e).__name__ == 'NotFound':
+            e.description = 'Not found'
+        response = {"error": e.description}
+        error_code = e.code
+    else:
+        response = {"error": e}
+        error_code = 500
+    return make_response(jsonify(response), error_code)
+
+
+if __name__ == "__main__":
+    """Runs the app in the command line"""
+    app.run(threade = True, host=host, port=port)
---
+++ api/v1/views/index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+A script that returns a json response
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
+
+
+@app_views.route('/status', methods = ['GET'])
+def status():
+    """A function that returns a JSON status: OK
+    """
+    if request.method == 'GET':
+        response = {"status": "OK"}
+        return jsonify(response)
+
+
+@app_views.route('/api/v1/stats', methods = ['GET'])
+def stats():
+    """A function that retrieves the number of each objects bu type"""
+    if request.method == 'GET':
+        response = {}
+        OBJECTS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for k, v in OBJECTS.items():
+            response[v] = storage.count(k)
+        return jsonify(response)
