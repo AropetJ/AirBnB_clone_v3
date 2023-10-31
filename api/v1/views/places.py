0239e7b2f37e44229a2daaa4d7792013bd2b9ed9
AropetJ <aropetjoel32@gmail.com>
2023-11-01 02:01:43 +0300

    Attempted task 10

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Places """
+"""
+Create a new view for Places objects that handles all
+default RESTFul API actions
+"""
 from models.state import State
 from models.city import City
 from models.place import Place
@@ -14,26 +17,21 @@ from flasgger.utils import swag_from
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 @swag_from('documentation/place/get_places.yml', methods=['GET'])
-def get_places(city_id):
-    """
-    Retrieves the list of all Place objects of a City
-    """
+def retrieve_places(city_id):
+    """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
 
     if not city:
         abort(404)
 
     places = [place.to_dict() for place in city.places]
-
     return jsonify(places)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/place/get_place.yml', methods=['GET'])
-def get_place(place_id):
-    """
-    Retrieves a Place object
-    """
+def retrieve_place(place_id):
+    """Retrieves a Place object based on the id"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -45,9 +43,7 @@ def get_place(place_id):
                  strict_slashes=False)
 @swag_from('documentation/place/delete_place.yml', methods=['DELETE'])
 def delete_place(place_id):
-    """
-    Deletes a Place Object
-    """
+    """Deletes a Place Object"""
 
     place = storage.get(Place, place_id)
 
@@ -63,10 +59,8 @@ def delete_place(place_id):
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 @swag_from('documentation/place/post_place.yml', methods=['POST'])
-def post_place(city_id):
-    """
-    Creates a Place
-    """
+def create_place(city_id):
+    """Creates a Place"""
     city = storage.get(City, city_id)
 
     if not city:
@@ -95,10 +89,8 @@ def post_place(city_id):
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/place/put_place.yml', methods=['PUT'])
-def put_place(place_id):
-    """
-    Updates a Place
-    """
+def update_place(place_id):
+    """Updates a Place using the id"""
     place = storage.get(Place, place_id)
 
     if not place:
@@ -119,9 +111,9 @@ def put_place(place_id):
 
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
 @swag_from('documentation/place/post_search.yml', methods=['POST'])
-def places_search():
+def search_places():
     """
-    Retrieves all Place objects depending of the JSON in the body
+    searches for all Place objects depending of the JSON in the body
     of the request
     """
 
