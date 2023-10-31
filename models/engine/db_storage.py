07490f6bfe21281a844d875dc53328eba4df2ec2
AropetJ <aropetjoel32@gmail.com>
2023-10-31 20:12:21 +0300

    Attempted task 1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,14 +3,18 @@
 Contains the class DBStorage
 """
 
-import json
+import models
 from models.amenity import Amenity
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+import os
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
