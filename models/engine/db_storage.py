91dfa7977c596a3f6b8e6739002ababa63627eb7
AropetJ <aropetjoel32@gmail.com>
2023-10-31 20:07:05 +0300

    Attempted task 1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,14 +3,14 @@
 Contains the class DBStorage
 """
 
-import os
-from models.base_model import Base
 from models.amenity import Amenity
+from models.base_model import Base
 from models.city import City
 from models.place import Place
-from models.state import State
 from models.review import Review
+from models.state import State
 from models.user import User
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
