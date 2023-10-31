fcd884b203773e454b3b02c6798197291899bbe7
AropetJ <aropetjoel32@gmail.com>
2023-10-31 18:36:20 +0300

    Attempted task 3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -50,7 +50,6 @@ def create_state():
     return make_response(jsonify(state.to_dict()), 201)
 
 
-
 @app_views.route('/states/<string:state_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_state(state_id):
