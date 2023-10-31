70ed5eb56bcc8127b292761468dd68e619685d81
AropetJ <aropetjoel32@gmail.com>
2023-11-01 00:26:47 +0300

    Attempeted task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,35 +72,33 @@ class DBStorage:
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """calls the remove() method on the private session attribute"""
         self.__session.remove()
 
     def get(self, cls, id):
         """
-        Returns the object based on the class name and its ID, or
-        None if not found
+        Retrieves an object based on the class name and its ID, or
+        None if the class is not found
         """
         if cls not in classes.values():
             return None
 
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
+        objcts = models.storage.all(cls)
+        for value in objcts.values():
             if (value.id == id):
                 return value
 
         return None
 
     def count(self, cls=None):
-        """
-        count the number of objects in storage
-        """
-        all_class = classes.values()
+        """Counts the number of objects in storage"""
+        all_objcts = classes.values()
 
         if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
+            i = 0
+            for clss in all_objcts:
+                i += len(models.storage.all(clss).values())
         else:
-            count = len(models.storage.all(cls).values())
+            i = len(models.storage.all(cls).values())
 
-        return count
+        return i
