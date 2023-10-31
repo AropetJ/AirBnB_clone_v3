dec870758b9a97eb6a950432c815c5c7b7cd351f
AropetJ <aropetjoel32@gmail.com>
2023-11-01 01:46:51 +0300

    Attempted task 8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" objects that handles all default RestFul API actions for Amenities"""
+"""
+Create a new view for amenity objects that handles all
+default RESTFul API actions
+"""
 from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
@@ -9,22 +12,20 @@ from flasgger.utils import swag_from
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/amenity/all_amenities.yml')
-def get_amenities():
-    """
-    Retrieves a list of all amenities
-    """
+def retrieve_amenities():
+    """Retrieves a list of all amenities"""
+    amenities = []
     all_amenities = storage.all(Amenity).values()
-    list_amenities = []
     for amenity in all_amenities:
-        list_amenities.append(amenity.to_dict())
-    return jsonify(list_amenities)
+        amenities.append(amenity.to_dict())
+    return jsonify(amenities)
 
 
 @app_views.route('/amenities/<amenity_id>/', methods=['GET'],
                  strict_slashes=False)
 @swag_from('documentation/amenity/get_amenity.yml', methods=['GET'])
-def get_amenity(amenity_id):
-    """ Retrieves an amenity """
+def retrieve_amenity(amenity_id):
+    """ Retrieves an amenity based on the id"""
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
@@ -36,10 +37,7 @@ def get_amenity(amenity_id):
                  strict_slashes=False)
 @swag_from('documentation/amenity/delete_amenity.yml', methods=['DELETE'])
 def delete_amenity(amenity_id):
-    """
-    Deletes an amenity  Object
-    """
-
+    """Deletes an amenity"""
     amenity = storage.get(Amenity, amenity_id)
 
     if not amenity:
@@ -47,16 +45,13 @@ def delete_amenity(amenity_id):
 
     storage.delete(amenity)
     storage.save()
-
     return make_response(jsonify({}), 200)
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 @swag_from('documentation/amenity/post_amenity.yml', methods=['POST'])
-def post_amenity():
-    """
-    Creates an amenity
-    """
+def create_amenity():
+    """Creates an amenity"""
     if not request.get_json():
         abort(400, description="Not a JSON")
 
@@ -72,10 +67,8 @@ def post_amenity():
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
 @swag_from('documentation/amenity/put_amenity.yml', methods=['PUT'])
-def put_amenity(amenity_id):
-    """
-    Updates an amenity
-    """
+def update_amenity(amenity_id):
+    """Updates an amenity"""
     if not request.get_json():
         abort(400, description="Not a JSON")
 
